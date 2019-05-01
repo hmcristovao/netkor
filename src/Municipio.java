822170a4156dd63df1f87fef9ae06e1f737a743a
Henrique <hmcristovao@gmail.com>
2019-05-01 00:45:26 -0300

    Lê o TXT com os municipios e o armazena em uma tabela hashing Acertado: alguns municípios estavam sem população.

--- src/Municipio.java
+++ src/Municipio.java
@@ -1,4 +1,57 @@
-
 public class Municipio {
-
+	private String nomeComEstado;
+	private String estado;
+	private String nome;
+	private String regiao;
+	private int populacao;
+	private String porte;
+	private boolean capital;
+	private int fator1;
+	private int fator2;
+	
+	public Municipio(String linha) {
+		String linhaSemAspas = linha.substring(1, linha.length()-1);
+		String trechoLinha[] = linhaSemAspas.split(",");
+		this.nomeComEstado 	= trechoLinha[0];
+		this.estado 		= trechoLinha[1];
+		this.nome 			= trechoLinha[2];
+		this.regiao 		= trechoLinha[3];
+		this.populacao 		= Integer.parseInt(trechoLinha[4]);
+		this.porte 			= trechoLinha[5];
+		this.capital		= trechoLinha[6].equals("Capital") ? true : false;
+		this.fator1 		= Integer.parseInt(trechoLinha[7]);
+		this.fator2 		= Integer.parseInt(trechoLinha[8]);
+	}
+	public String getNomeComEstado() {
+		return this.nomeComEstado;
+	}
+	public String getEstado() {
+		return this.estado;
+	}
+	public String getNome() {
+		return this.nome;
+	}
+	public String getRegiao() {
+		return this.regiao;
+	}
+	public int getPopulacao() {
+		return this.populacao;
+	}
+	public String getPorte() {
+		return this.porte;
+	}
+	public boolean getCapital() {
+		return this.capital;
+	}
+	public int getFator1() {
+		return this.fator1;
+	}
+	public int getFator2() {
+		return this.fator2;
+	}
+	public String toString() {
+		return this.getNomeComEstado()+","+this.getEstado()+","+this.getNome()+","+this.getRegiao()
+				+","+this.getPopulacao()+","+this.getPorte()+","+this.getCapital()+","+this.getFator1()
+				+","+this.getFator2();
+	}		
 }
--- src/Uso.java
+++ src/Uso.java
@@ -1,23 +1,49 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+// Tarefas:	
+
+// Henrique
+// ler o TXT principal e carregá-lo em uma tabela hashing
+
+// Keicila
+// criar uma tabela hashing com as divisas
+
+// Klaus
+// percorrer a tabela e gerar os vertices, particões e vetores
+
+// Luiz
+// percorrer as tabelas e gerar os arcs
 
 public class Uso {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String linha;
+		BufferedReader arqEntrada;
+		Municipio municipio = null;
+		HashMap<String, Municipio> tabela = new HashMap<String, Municipio>();
+		try {
+			// abre o arquivo para leitura no modo UNICODE (o mesmo formato TXT gerado pelo Excel)
+			arqEntrada = new BufferedReader(new InputStreamReader(new FileInputStream("municipios_divisas.txt"), "UNICODE"));
+			// percorre o arquivo, linha a linha, armazenando-o na tabela hashing
+			while(true) {
+				linha = arqEntrada.readLine();
+				if(linha == null) 
+					break;
+				municipio = new Municipio(linha);
+				tabela.put(municipio.getNomeComEstado(), municipio);
+			} 
+			arqEntrada.close();
 			
-		// Henrique
-		// ler o TXT principal e carregá-lo na tabela hash
-		
-		// Keicila
-		// criar uma tabela hash com as divisas
-		
-		// Klaus
-		// percorrer a tabela e gerar os vertices, particoes e vetores
-		
-		// Luiz
-		// percorrer as tabelas e gerar os arcs
-		
-	
+			// apenas para testar, exibe o conteúdo da tabela hashing:
+			System.out.println(tabela);
+			
+		} 
+		catch(IOException e) {
+			System.out.println("erro no acesso ao arquivo");
+		}		
 	}
-
 }
