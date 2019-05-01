a0385e1a5481de0ca2675f809debc64c080d427a
Henrique <hmcristovao@gmail.com>
2019-04-30 23:09:45 -0300

    Distribuição das tarefas na reunião do dia 30-abril

---
+++ src/Uso.java
@@ -0,0 +1,23 @@
+
+public class Uso {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			
+		// Henrique
+		// ler o TXT principal e carregá-lo na tabela hash
+		
+		// Keicila
+		// criar uma tabela hash com as divisas
+		
+		// Klaus
+		// percorrer a tabela e gerar os vertices, particoes e vetores
+		
+		// Luiz
+		// percorrer as tabelas e gerar os arcs
+		
+	
+	}
+
+}
